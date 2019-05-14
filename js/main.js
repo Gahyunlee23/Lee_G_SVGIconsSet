d394fc4a43c994749728c6bd5355600d8d712e10
Lee <g_lee89360@tss-hr420-dm10.local>
2019-05-14 17:57:54 -0400

    make some changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 (() => {
-    //
+    //stub
     console.log('fired!');
+
+    //select elements with JavaScript
+    // setting up a variable using the let keyboard,
+    //and using a CSS Selector to find the elenent we want to use.
+
+    //let stores the reference to element as a variable (in memory)
+    let svgGraphic = document.querySelector('#badgeSVG');
+    //functions are reuseable piece of code
+    //that ypu can run ant time
+    function logMyId() {
+      console.log(this.id);
+
+      this.style.opacity = 0.5;
+    }
+
+    svgGraphic.addEventListener("click", logMyId)
+
 
 })();
