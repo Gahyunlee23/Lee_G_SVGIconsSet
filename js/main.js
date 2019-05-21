a9fbed900fe9a26456bc0bd10e04821bcbbb26c3
Lee <g_lee89360@tss-hr420-dm09.fc.ca>
2019-05-21 16:51:40 -0400

    adding changes

--- js/main.js
+++ js/main.js
@@ -7,16 +7,30 @@
     //and using a CSS Selector to find the elenent we want to use.
 
     //let stores the reference to element as a variable (in memory)
-    let svgGraphic = document.querySelector('#badgeSVG');
+    let svgGraphic = document.querySelector('#badgeSVG'),
+        mainHeadline = document.querySelector('.main-headline'),
+        subHeadline = document.querySelector('.sub-headline'),
+        swapTextBotton = document.querySelector('.switch-type');
+
     //functions are reuseable piece of code
-    //that ypu can run ant time
+    //that ypu can run any time
     function logMyId() {
       console.log(this.id);
 
       this.style.opacity = 0.5;
     }
 
-    svgGraphic.addEventListener("click", logMyId)
+    function swapText() {
+        mainHeadline.textContent = "Now You're something else!";
+        mainHeadline.classList.toggle("selected");
+
+        subHeadline.textContent = "Magic! Change!";
+        subHeadline.classList.toggle("selected");
+    }
+
+    //events always go down here
+    svgGraphic.addEventListener("click", logMyId);
+    swapTextBotton.addEventListener("click",swapText);
 
 
 })();
