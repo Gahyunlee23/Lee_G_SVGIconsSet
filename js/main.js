2521717639223cfc110579b4a921d8b502706c12
Stella <stella@MacBook-Pro.local>
2019-05-25 17:10:04 -0400

    some java update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 (() => {
     //stub
-    console.log('fired!');
-
+    console.log('working!');
+    
+    //Bring(let) Section
     let mainLine = document.querySelector(".main-title")
         convertButton = document.querySelector(".magic Button")
         loadButton = document.querySelector(".image-container Button")
 
-
+    // Function
     function changeText() {
         mainLine.textContent = "Fruit icons for SVG Assignment!!";
         mainLine.classList.toggle("selected");
@@ -17,7 +18,7 @@
         console.log(this.previousElementSibling.id);
     }
 
-
+    //Event Section
     convertButton.addEventListener("click", changeText);
    
     loadButton.addEventListener("click", loadTheSVG);
