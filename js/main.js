c6e01e07ed19b2bad79c976bf772e26f9b9b6ed1
Stella <stella@MacBook-Pro.local>
2019-05-25 01:19:38 -0400

    SVG iCONS update

--- js/main.js
+++ js/main.js
@@ -2,44 +2,18 @@
     //stub
     console.log('fired!');
 
-    //select elements with JavaScript
-    // setting up a variable using the let keyboard,
-    //and using a CSS Selector to find the elenent we want to use.
+    let mainLine = document.querySelector(".main-title")
+        convertButton = document.querySelector(".magic Button")
 
-    //let stores the reference to element as a variable (in memory)
-    let mainHeadline = document.querySelector(".main-headline"),
-        subHeadline = document.querySelector(".sub-headline"),
-        swapTextBotton = document.querySelector(".switch-type"),
-        allImages = document.querySelectorAll("#image-container img"),
-        objectBotton = document.querySelector(".container button");
+        
 
 
-    //functions are reuseable piece of code
-    //that ypu can run any time
-    function logMyId() {
-      console.log(this.id);
-      //toggle a class here with classList
+    function changeText() {
+        mainLine.textContent = "Fruit icons for SVG Assignment!!";
+        mainLine.classList.toggle("selected");
     }
 
-    function swapText() {
-        mainHeadline.textContent = "Now You're something else!";
-        mainHeadline.classList.toggle("selected");
+    convertButton.addEventListener("click", changeText);
 
-        subHeadline.textContent = "Magic! Change!";
-        subHeadline.classList.toggle("selected");
-    }
 
-    function logTheSVG() {
-      console.log(this.previousElementSibling.id);
-    }
-
-    //events always go down here
-    swapTextBotton.addEventListener("click",swapText);
-
-    // select and loop through a bunch of otems ay once --> one to many relationship
-    allImages.forEach(item => {
-      item.addEventListener("click", logMyId);
-    });
-
-    objectBotton.addEventListener("click", logTheSVG);
-    })();
+})();
